329bbb7cb873d32b54c227b52c89e95c8bfdfd50
youngjun827 <youngjunjoo827@gmail.com>
2023-11-07 17:38:58 -0500

    refactor(web): Improve error handling and shutdown logic in web framework

--- foundation/web/web.go
+++ foundation/web/web.go
@@ -3,8 +3,10 @@ package web
 
 import (
 	"context"
+	"errors"
 	"net/http"
 	"os"
+	"syscall"
 	"time"
 
 	"github.com/go-chi/chi/v5"
@@ -26,6 +28,22 @@ func NewApp(shutdown chan os.Signal, mw ...Middleware) *App {
         shutdown: shutdown,
 		mw: mw,
     }
+}
+
+func (a *App) SignalShutdown() {
+	a.shutdown <- syscall.SIGTERM
+}
+
+func validateShutdown(err error) bool {
+	switch {
+	case errors.Is(err, syscall.EPIPE):
+		return false
+
+	case errors.Is(err, syscall.ECONNRESET):
+		return false
+	}
+
+	return true
 }
 
 func HandlerAdapter(handler Handler) http.HandlerFunc {
@@ -50,10 +68,15 @@ func (a *App) Handle(method string, path string, handler Handler, mw ...Middlewa
 		ctx = SetValues(ctx, &v)
 		err := wrappedHandler(ctx, w, r)
         if err != nil {
-            http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			if validateShutdown(err) {
+				a.SignalShutdown()
+				return err
+			}
         }
         return nil
     }
 
     a.Mux.MethodFunc(method, path, HandlerAdapter(customHandler))
 }
+
+
