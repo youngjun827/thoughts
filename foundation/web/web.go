260098a29733893bba6883221a821e2ee1e13cf0
youngjun827 <youngjunjoo827@gmail.com>
2023-11-07 16:32:19 -0500

    feat(api): Implement middleware support in web framework extension

--- foundation/web/web.go
+++ foundation/web/web.go
@@ -14,13 +14,15 @@ type Handler func(context.Context, http.ResponseWriter, *http.Request) error
 type App struct {
     Mux *chi.Mux
     shutdown chan os.Signal
+	mw []Middleware
 }
 
-func NewApp(shutdown chan os.Signal) *App {
+func NewApp(shutdown chan os.Signal, mw ...Middleware) *App {
     r := chi.NewRouter()
     return &App{
         Mux:      r,
         shutdown: shutdown,
+		mw: mw,
     }
 }
 
@@ -33,6 +35,18 @@ func HandlerAdapter(handler Handler) http.HandlerFunc {
         }
     }
 }
-func (a *App) Handle(method string, path string, handler Handler) {
-    a.Mux.MethodFunc(method, path, HandlerAdapter(handler))
+
+func (a *App) Handle(method string, path string, handler Handler, mw ...Middleware) {
+    wrappedHandler := wrapMiddleware(mw, handler)
+    wrappedHandler = wrapMiddleware(a.mw, wrappedHandler)
+
+    customHandler := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+		err := wrappedHandler(ctx, w, r)
+        if err != nil {
+            http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        }
+        return nil
+    }
+
+    a.Mux.MethodFunc(method, path, HandlerAdapter(customHandler))
 }
